7be4852c13ccc265d597dce9d571218ba340a68a
keatonwilson <keatonwilson@me.com>
2019-03-20 14:28:35 -0700

    Full workthrough (blockCV -> ENMEval with hyperparameter tuning) with all data (no splitting yet).

--- scripts/blockCV.R
+++ scripts/blockCV.R
@@ -106,8 +106,16 @@ a_train = train_data %>%
   filter(Species == 0) %>%
   select(longitude, latitude)
 
+p_test = test_data %>%
+  filter(Species == 1) %>%
+  select(longitude, latitude)
+
+a_test = test_data %>%
+  filter(Species == 0) %>%
+  select(longitude, latitude)
+
 #First attempt at a model - took ~115 minutes to run
-eval_1 = ENMevaluate(occ = p_train, bg.coords = a_train, env = bioclim, method = 'randomkfold', kfolds = 5)
+eval_1 = ENMevaluate(occ = p_train, bg.coords = a_train, env = bioclim, method = 'randomkfold', kfolds = 5, algorithm = 'maxent.jar')
 
 #Best model according to one metric - average AUC on test data - LQHPT with a 0.5 regularization multiplier
 eval_1@results[which(eval_1@results$avg.test.AUC== max(eval_1@results$avg.test.AUC)),]
@@ -116,14 +124,66 @@ eval.plot(eval_1@results, 'avg.test.AUC', var = 'var.test.AUC')
 #So we can make predictions on the spatially explicit test data now.
 eval_1@results[which(eval_1@results$avg.test.AUC== max(eval_1@results$avg.test.AUC)),]
 
-#Predictions
-big_test_p = predict(eval_1@models[[6]], newdata = test_data, type = "cloglog")
+#Evaluate on test data
+ev = evaluate(p_test, a = a_test,  model = eval_1@models[[6]], x = bioclim.data)
+plot(ev, 'ROC')
 
-#Building ROC curve and calculating AUC on the test data
-library(ROCR)
-pred=prediction(big_test_p, test_data$Species)
+#Let's build the final model
+aicmods <- which(eval_1@results$AICc == min(na.omit(eval_1@results$AICc)))[1] # AIC model
+aicmods = eval_1@results[aicmods,]
+FC_best = as.character(aicmods$features[1])
+rm_best = aicmods$rm
 
-perf=performance(pred,"tpr","fpr")
-plot(perf)
-performance(pred,"auc")
+maxent.args = ENMeval::make.args(RMvalues = rm_best, fc = FC_best)
+mx_best = maxent(bioclim.data, as.matrix(swallowtail[,1:2]), args = maxent.args[[1]])
 
+#Predictions from full model
+# Determine geographic extent of our data
+max_lat_swallowtail <- ceiling(max(swallowtail$latitude))
+min_lat_swallowtail <- floor(min(swallowtail$latitude))
+max_lon_swallowtail <- ceiling(max(swallowtail$longitude))
+min_lon_swallowtail <- floor(min(swallowtail$longitude))
+geographic.extent <- extent(x = c(min_lon_swallowtail, max_lon_swallowtail, min_lat_swallowtail, max_lat_swallowtail))
+
+predict_presence = dismo::predict(object = mx_best, x = bioclim.data, ext = geographic.extent)
+
+pred_sp <- as(predict_presence, "SpatialPixelsDataFrame")
+pred_sp_df <- as.data.frame(pred_sp)
+colnames(pred_sp_df) <- c("value", "x", "y")
+
+#Pulling in polygons for states and provinces
+#Getting map data
+usa = getData(country = 'USA', level = 1)
+
+#extract states (need to uppercase everything)
+to_remove = c("Alaska", "Hawaii", "North Dakota", "South Dakota", "Montana", 
+              "Wyoming", "Idaho", "Washington", "Oregon", "Nevada", "California", 
+              "Arizona", "Utah", "New Mexico", "Colorado", "Nebraska", "Texas", 
+              "Oklahoma", "Kansas")
+
+#filtering
+mapping = usa[-match(toupper(to_remove), toupper(usa$NAME_1)),]
+
+#simplying polygons
+simple_map_US = gSimplify(mapping, tol = 0.01, topologyPreserve = TRUE)
+
+#Pulling Canada Province data
+can = getData(country = 'CAN', level = 1)
+province = c("Ontario")
+can_mapping = can[match(toupper(c("Ontario", "Québec")), toupper(can$NAME_1)),]
+simple_map_can = gSimplify(can_mapping, tol = 0.01, topologyPreserve = TRUE)
+
+#Plotting - T1 First
+g1 = ggplot() +  
+  geom_polygon(data=simple_map_US, aes(x=long, y=lat, group=group), 
+               color=NA, size=0.25, fill = "#440154FF") +
+  geom_polygon(data = simple_map_can, aes(x = long, y = lat, group = group), color = NA, size = 0.25, fill = "#440154FF") +
+  geom_tile(data=pred_sp_df, aes(x=x, y=y, fill=value)) + 
+  geom_polygon(data=simple_map_US, aes(x=long, y=lat, group=group), 
+               color="grey50", size=0.25, fill = NA) +
+  geom_polygon(data = simple_map_can, aes(x = long, y = lat, group = group), color = "grey50", size = 0.25, fill = NA) +
+  scale_fill_viridis() +
+  theme(legend.position="bottom") +
+  theme(legend.key.width=unit(2, "cm")) +
+  coord_equal(ylim = c(22, 50), xlim = c(-100, -65)) +
+  theme_map()
